c863905a86eb93fb9dc82d40b6efa32c7a7dae03
Asher <asher@ridiculouspower.com>
2011-04-07 04:19:50 -0400

    session ready for spec/testing

---
+++ spec/Rmagnets/Rmagnets_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../../lib/rmagnets-session.rb'
+
+describe Rmagnets::Session do
+
+  ##################
+  #  session_port  #
+  ##################
+  
+  it "" do
+    pending
+  end
+
+end
